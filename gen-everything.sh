87e5271528597e8d58a38ef93ff039bd1c718e1e
Victor Miraldo <victor.miraldo@oracle.com>
2019-11-27 09:37:57 +0100

    Made a script to generate the 'Everything' file; this file will not be under version control though

---
+++ gen-everything.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+echo "module Everything where" > Everything.agda
+find LibraBFT/ -name "*.agda" \
+  | sed 's/\.agda//' \
+  | sed 's!/!\.!g' \
+  | sed 's/^/  open import /' >> Everything.agda
+
+  
