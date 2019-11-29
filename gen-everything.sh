abb2d5e0358bdd9689298358db71412cfc26341b
Mark Moir <mark.moir@oracle.com>
2019-11-29 16:27:52 +1300

    Fix small bug in gen-everything.sh

--- gen-everything.sh
+++ gen-everything.sh
@@ -3,6 +3,7 @@
 echo "module Everything where" > Everything.agda
 find LibraBFT/ -name "*.agda" \
   | sed 's/\.agda//' \
+  | sed 's!//!/!g' \
   | sed 's!/!\.!g' \
   | sed 's/^/  open import /' >> Everything.agda
 
