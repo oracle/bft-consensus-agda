00fea51b26286c3690bb0c0be7a7a418b735719e
Mark Moir <mark.moir@oracle.com>
2022-06-30 12:52:47 +1200

    Make gen-everything include .agda files under src, other than Everything.agda
    Signed-off-by: Mark Moir <mark.moir@oracle.com>

--- Scripts/gen-everything.sh
+++ Scripts/gen-everything.sh
@@ -5,14 +5,20 @@
 # Copyright (c) 2020 Oracle and/or its affiliates.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl/
 
-echo "module Everything where" > src/Everything.agda
-for f in $(find src -type d -maxdepth 1 -mindepth 1)
+# This script generates a module that imports every .agda file
+# under src, so we can typecheck all files with one command
+# (see run-everything.sh)
+echo "module Everything where" > tmp.$$
+for f in $(find src -name "*.agda")
 do
-    find "$f" -name "*.agda" \
+        echo $f                       \
       | sed 's/\.agda//' \
       | sed 's/src.//' \
       | sed 's!/!\.!g' \
-      | sed 's/^/open import /' >> src/Everything.agda
+      | sed 's/^/open import /' >> tmp.$$
 done
 
+# Well, not quite *everything*
+grep -v "open import Everything" tmp.$$ > src/Everything.agda
+rm tmp.$$
 
